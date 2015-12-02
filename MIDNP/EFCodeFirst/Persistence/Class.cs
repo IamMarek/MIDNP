c0b959386b68a53fdcf4c02df775d30d2d2dffdc
MIDNP <skotnicam@gmail.com>
2015-12-02 21:36:04 +0100

    EF Code First

---
+++ MIDNP/EFCodeFirst/Persistence/Class.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EFCodeFirst.Persistence
+{
+    public class Class
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ MIDNP/EFCodeFirst/Persistence/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirst.Persistence
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Class> Classes { get; set; }
+    }
+}
